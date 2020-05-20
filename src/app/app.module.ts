a94b1cbf4c9172d4614e5f18e0142cf02199454c
Raiosilva <raimundopi025@gmail.com>
2020-05-19 22:29:27 -0300

    setup initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MainModule } from './main/main.module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainModule,
+    BrowserModule,
+    MaterialModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
